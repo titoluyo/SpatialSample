0b1d8cb74c16ddfb4cc8863ead5ac238e3ec3e9c
Tito Takeo Luyo Murata <tluyo@wordstream.com>
2020-08-08 01:02:56 -0500

    add reac app

---
+++ app/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import PersistentDrawerRight from './Drawer';
+
+function App() {  
+  return (
+    <PersistentDrawerRight></PersistentDrawerRight>
+  );
+}
+
+export default App;
---
+++ app/client/src/Maps.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+  //  InfoWindow,
+  } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+height: "100vh",
+width: "100vw",
+};
+const center = {
+lat: -12.119274,
+lng: -77.029075,
+};
+
+function Maps() {
+    console.log('process.env.REACT_APP_GOOGLE_MAPS_API_KEY');
+    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+
+    if (loadError) return <div>Error</div>;
+    if (!isLoaded) return <div>Loading...</div>;
+
+    return <GoogleMap
+            id="map"
+            mapContainerStyle={mapContainerStyle}
+            zoom={16}
+            center={center}
+            >
+                <Marker
+                    key={`Miraflores`}
+                    position={center}
+                >
+                </Marker>
+            </GoogleMap>
+}
+
+export default Maps;
