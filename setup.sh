f0d5f563512e43f14824e3cf04384969b95ee8ed
Tito Luyo Murata <tluyo@hotmail.com>
2021-12-26 18:00:29 -0500

    changes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 # # req (virtualenv)
-# sudo apt-get install virtualenv
+# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+# windows
+# py -m pip install --upgrade pip     # upgrade pip
+# py -m pip --version                 # check pip version
+# py -m pip install --user virtualenv # install virtualenv
+# py -m venv spatial                  # create virtualenv "spatial"
+# .\env\Scripts\activate              # activate virtualenv
+
+
+
 # # req (psycopg2)
 # sudo apt-get install --reinstall libpq-dev
 
 safe_workon() {
-  source "$HOME/.virtualenvs/$1/bin/activate"
+  source "./$1/Scripts/activate"
 }
 if ! [ -f gdrive.sh ]; then
     echo '# downloading gdrive.sh ...'
@@ -17,13 +26,16 @@ fi
 if ! [ -d geodata ]; then
     tar -xvzf geodata.tar.gz
 fi
-if ! [ -d ~/.virtualenvs/spatial ]; then
-    virtualenv -p `which python3` ~/.virtualenvs/spatial
-fi
+# if ! [ -d ~/.virtualenvs/spatial ]; then
+#     virtualenv -p `which python3` ~/.virtualenvs/spatial
+# fi
 sleep 5
-safe_workon spatial
-pip install -r app/server/requirements.txt
+# echo '# activating virtual env spatial'
+# safe_workon spatial
+# echo '# installing requirements ...'
+# pip install -r app/server/requirements.txt
 
+echo '# docker compose up'
 #docker-compose -f docker-compose.yml -f docker-compose.debug.yml up -d
 docker-compose up -d
 echo "waiting..."
@@ -36,17 +48,22 @@ if ! docker exec spatialdb psql -U postgres -c "\l" | grep spatialdb; then
 fi
 echo "loading maps ..."
 if ! docker exec spatialdb psql -U postgres -d spatialdb -c "\dt" | grep departamentos; then
+    echo "loading DEPARTAMENTOS"
     docker exec -i spatialdb bash -c "shp2pgsql /geodata/departamentos/DEPARTAMENTOS.shp public.departamentos | psql -h localhost -d spatialdb -U postgres"
 fi
 if ! docker exec spatialdb psql -U postgres -d spatialdb -c "\dt" | grep provincias; then
+    echo "loading PROVINCIAS"
     docker exec -i spatialdb bash -c "shp2pgsql /geodata/provincias/PROVINCIAS.shp public.provincias | psql -h localhost -d spatialdb -U postgres"
 fi
 if ! docker exec spatialdb psql -U postgres -d spatialdb -c "\dt" | grep distritos; then
+    echo "loading DISTRITOS"
     docker exec -i spatialdb bash -c "shp2pgsql /geodata/distritos/DISTRITOS.shp public.distritos | psql -h localhost -d spatialdb -U postgres"
 fi
 if ! docker exec spatialdb psql -U postgres -d spatialdb -c "\dt" | grep usa_counties; then
+    echo "loading USA_COUNTIES"
     docker exec -i spatialdb bash -c "shp2pgsql /geodata/USA_shp/USA_counties.shp public.usa_counties | psql -h localhost -d spatialdb -U postgres"
 fi
 if ! docker exec spatialdb psql -U postgres -d spatialdb -c "\dt" | grep world_countries; then
+    echo "loading WORLD_COUNTRIES"
     docker exec -i spatialdb bash -c "shp2pgsql /geodata/World_Countries/World_Countries.shp public.world_countries | psql -h localhost -d spatialdb -U postgres"
 fi
